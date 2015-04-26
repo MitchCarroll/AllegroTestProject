9256d6fb25ee6d76160614a42160400ca793775e
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2015-04-26 16:09:15 -0500

    Added CMakeLists.txt and initialized CMake project

---
+++ CMakeFiles/altest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/altest.dir/src/test.cpp.o"
+  "altest.pdb"
+  "altest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/altest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

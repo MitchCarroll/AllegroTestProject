861f1c2b7fe587cb73a54c9fafbadd9be2940e61
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2015-04-26 21:58:58 -0500

    tweaks and bugfixes.

--- src/test.cpp
+++ src/test.cpp
@@ -3,22 +3,23 @@
    this file is part of a test project which can be used as boilerplate
    for making new CMake/Allegro5 projects
 
-   I have yet to figure out some more advanced topics, like Android deployment.
+   I have yet to figure out some more advanced topics, like Android 
+   deployment.
    As of right now this works on Linux for sure :-)
 */
 
-#include <allegro5/allegro.h>          //must be included by any allegro program
-#include <allegro5/allegro_image.h>    //allows us to load images and such
-#include <iostream>                    //just for my error message
+#include <allegro5/allegro.h> //must be included by any allegro program
+#include <allegro5/allegro_image.h> //allows us to load images and such
+#include <iostream> //just for my error message
 //incidentally, this should really be done with an allegro messagebox
-using namespace std; //because I really hate typing std:: before stuff...
+using namespace std; //because I really hate typing std:: before stuff.
 
 int main(int argc, char **argv)
 //Allegro requires your main function to have this signature
 {
   al_init(); //initialize allegro itself
-   
-  al_set_new_display_flags(ALLEGRO_WINDOWED); //do this stuff before creating
+  al_set_new_display_flags(ALLEGRO_WINDOWED);
+  //do this stuff before creating
                                               //the new window
   ALLEGRO_DISPLAY *screen=al_create_display(800,600); //800X600 window
   al_set_window_title(screen,"Test"); //sets the text of the titlebar
@@ -34,21 +35,31 @@ int main(int argc, char **argv)
   
   ALLEGRO_BITMAP *my_pic = NULL; //allocate memory for an image
   my_pic = al_load_bitmap(DATADIR "test.png"); //load the image
-  //NOTE: DATADIR is a constant which is set by our CMake setup at compile time
-  //it is a string that is tacked onto the front of the filename so the program
+  //NOTE: DATADIR is a constant which is set by our CMake setup
+  //at compile time
+  //it is a string that is tacked onto the front of the filename so the
+  //program
   //can find it (after installing)
   //e.g.: "/usr/local/share/AllegroTestProgram/data/"
-  //this way, instead of typing someting like load("myFile.dat") you can do:
+  //this way, instead of typing someting like load("myFile.dat") you
+  //can do:
   //load(DATADIR "myFile.dat")
   al_clear_to_color(al_map_rgb_f(.5,.5,.5));
   if(!my_pic) //if the file doesn't load right, or isn't found
-    cout << DATADIR << "test.png" << " DIDN'T LOAD!" << endl; //print an error
+    cout << DATADIR << "test.png" << " DIDN'T LOAD!" << endl;
+  //print an error
   else {
     al_draw_bitmap(my_pic, 0, 0, 0); //draw the image to the window
-    al_draw_scaled_rotated_bitmap(my_pic, 256,256, 600,200, .5,1.5, 45, 0);
+    al_draw_scaled_rotated_bitmap(my_pic,
+				  256,256,
+				  600,200,
+				  0.5, 1.5,
+				  45,
+				  0);
   }
-
-  al_flip_display(); //flip the backbuffer to the screen so we can see it
+  
+  al_flip_display();
+  //flip the backbuffer to the screen so we can see it
 
   bool done=false; //this just stores whether we want to quit yet or not
   while(!done) 
@@ -64,7 +75,8 @@ int main(int argc, char **argv)
 	    case ALLEGRO_KEY_ESCAPE: //and it's the ESC key...
 	      done = true; //then we are done
 	      break;
-	    default: //it's good practice to always define a default case
+	    default:
+	      //it's good practice to always define a default case
 	      break;
 	    }
 	}
